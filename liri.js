53ffe7a9a3cb471e4600bdc03c8dc7812bbae557
Jeff Hinzmann <JAHinzmann@gmail.com>
2018-04-24 15:18:03 -0400

    fixed problems with API requests

--- liri.js
+++ liri.js
@@ -2,12 +2,14 @@
 //import { parse } from "querystring";
 
 //Dependencies
-// var env = require("dotenv").config();
+require("dotenv").config();
 var fs = require('fs') //file system node library allows file-system interactivity
 var request = require('request'); //request node library
 var LiriTwitterBOT = require("./keys.js"); //store data from keys.js
 var twitter = require('twitter'); //twitter node library
-var spotify = require('./keys.js');
+var Spotify = require('node-spotify-api'); //spotify node library
+
+// console.log(LiriTwitterBOT);
 
 //log function
 
@@ -27,7 +29,8 @@ function writeLog(textLog) {
 // capture inputs with argv[] and join
 
 var input = process.argv[2];
-var query = process.argv.slice(3) + join('+');
+var query = process.argv.slice(3); 
+query.join('+');
 // for (i = 3; i < process.argv; i++) {
 //     query += process.argv[i];
 // }
@@ -35,23 +38,23 @@ var query = process.argv.slice(3) + join('+');
 // create code for the paramters regarding the pulls from each of the API's
 // Twitter
 
-var twitterClient = new twitter(LiriTwitterBOT.LiriTwitterBOT);
+var twitterClient = new twitter(LiriTwitterBOT.twitter);
 var twitterParams = {
     screen_name: 'Hinzmannator',
     count: 20
 };
+
 //getTweets function
 function getTweets() {
     twitterClient.get('statuses/user_timeline', twitterParams, function(error, response) {
         if (error) {
-            console.log(err)
+            console.log(error)
         };
-        console.log(response);
         console.log("these are my last" + response.length + "tweets.")
         for (var i = 0; i < response.length; i++) {
             console.log('#' + (i + 1) + " = " + response[i].text);
             console.log('posted on: ' + response[i].created_at);
-            logObject = task + "" + logObject + "\n";
+            logObject = input + "" + response[i].text + "\n";
             writeLog(logObject);
         }
     });
@@ -60,24 +63,35 @@ function getTweets() {
 //Spotify
 
 
+
 //getSongs function
-function getSongs(query) {
-    if (!query) {
-        query = "what's+my+age+again";
+function getSongs(Query) {
+    var spotify = new Spotify(LiriTwitterBOT.spotify);
+    if (!Query) {
+        Query = "what's+my+age+again";
     };
-    var queryURL = 'https://api.spotfiy.com/v1/search?q=' + query + '&limit=5&type=track';
-    request(queryURL, function(err, response, body) {
-        if (err) {
-            console.log(err);
-        };
-        body = JSON.parse(body);
-        console.log('tunes for your ears motherfucker');
-        for (var i = 0; i < body.tracks.items.length; i++) {
-            logObject = input + ", " + query + ", " + body.tracks.items[i].artists[0].name + ", " + body.tracks.items[i].name
-            body.tracks.items[i].name + ", " + body.tracks.items[i].preview_url + ", " + body.tracks.items[i].album.name + "\n";
-        }; //end of loop
-        writeLog(logObject);
+    spotify.search({ type: 'track', query: Query }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+     console.log(data);
+        // Do something with 'data' 
     });
+
+    // var queryURL = 'https://api.spotfiy.com/v1/search?q=' + query + '&limit=5&type=track';
+    // request(queryURL, function(err, response, body) {
+    //     if (err) {
+    //         console.log(err);
+    //     };
+        // body = JSON.parse(body);
+        // console.log(body);
+        // for (var i = 0; i < body.tracks.items.length; i++) {
+        //     logObject = input + ", " + query + ", " + body.tracks.items[i].artists[0].name + ", " + body.tracks.items[i].name
+        //     body.tracks.items[i].name + ", " + body.tracks.items[i].preview_url + ", " + body.tracks.items[i].album.name + "\n";
+        // }; //end of loop
+        // writeLog(logObject);
+    // });
 }; //End of Spotify
 
 //OMDB
